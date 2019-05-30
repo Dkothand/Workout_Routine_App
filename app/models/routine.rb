19d102a35bc417e8e0f87481c146045a7711f4eb
Deva <dkothand@gmail.com>
2019-05-30 13:40:22 -0400

    commented out cascade delete as it removes the item from database when we want to just delete the association

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,5 @@
 class Routine < ApplicationRecord
-    has_many :workouts, :dependent => :destroy
+    # has_many :workouts, :dependent => :destroy
+    has_many :workouts
     has_many :exercises, through: :workouts
 end
